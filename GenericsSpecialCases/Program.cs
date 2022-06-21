9c838595f5747e668e1508f9da8156bcbeb72c54
SaileshBK <Sailesh.bk@bngholdingsinc.com>
2022-06-21 10:10:28 -0500

    use Static member in Generic classes.

--- GenericsSpecialCases/Program.cs
+++ GenericsSpecialCases/Program.cs
@@ -6,7 +6,32 @@ namespace GenericsSpecialCases
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // we have here 2 instances of container of string, 1 instances of container of int and 3 instance of containerBase.
+            _ = new Container<string>();
+            _ = new Container<string>();
+            _ = new Container<int>();
+
+            Console.WriteLine($"Container<string>: {Container<string>.InstanceCount}");
+            Console.WriteLine($"Container<int>: {Container<int>.InstanceCount}");
+            Console.WriteLine($"Container<bool>: {Container<bool>.InstanceCount}");
+            Console.WriteLine($"ContainerBase: {ContainerBase.InstanceCountBase}");
+
+            Console.ReadLine();
         }
+    }
+    class ContainerBase
+    {
+        public ContainerBase() => InstanceCountBase++;
+
+
+        public static int InstanceCountBase { get; private set; }
+    }
+
+    class Container<T> : ContainerBase
+    {
+        public Container() => InstanceCount++;
+        
+
+        public static int InstanceCount { get; private set; }
     }
 }
